ede67f5655cae8a216366aaae91d73daa192abb0
jinhyung <rheejinhyung@gmail.com>
2024-04-06 14:50:45 +0900

    [String Class]요구사항2 반영

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -20,4 +20,10 @@ public class StringTest {
         String[] actual2 = "1".split(",");
         assertThat(actual2).containsExactly("1");
     }
+
+    @Test
+    void 요구사항2() {
+        String actual = "(1,2)".substring(1,4);
+        assertThat(actual).isEqualTo("1,2");
+    }
 }
