95f34e7fe4eb5d2d8f363395ff4e9b8ecd977cda
jinhyung <rheejinhyung@gmail.com>
2024-04-07 21:23:14 +0900

    [Set Collection]요구사항2 반영

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -2,6 +2,8 @@ package study;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -24,6 +26,19 @@ public class SetTest {
     @Test
     void 요구사항1() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints={1, 2, 3})
+    void 요구사항2(int number) {
+        assertThat(numbers.contains(number)).isTrue();
     }
 
 }
