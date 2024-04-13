f774f9c915102e99c92cecab0b8af61822da1eeb
jinhyung <rheejinhyung@gmail.com>
2024-04-13 15:18:50 +0900

    feat 기능2-사용자로부터 숫자 입력받아 동일한지 판별하는 로직 추가

--- src/main/java/TDD/BaseballGame.java
+++ src/main/java/TDD/BaseballGame.java
@@ -1,17 +1,33 @@
 package TDD;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class BaseballGame {
 
     public static void main(String[] args) {
-        System.out.println(generateRandomNumber());
+
+        Scanner scanner = new Scanner(System.in);
+        String targetNubmer = generateRandomNumber();
+        System.out.println(targetNubmer);
+
+        while (true) {
+
+            System.out.print("숫자를 입력해주세요 : ");
+            String inputNumber = scanner.nextLine();
+            System.out.println("INPUT : " + inputNumber);
+
+            IsCorrect(targetNubmer, inputNumber);
+
+        }
+
+
+
     }
 
     public static String generateRandomNumber() {
 
         Random random = new Random();
-
         int number1 = 0;
         int number2 = 0;
         int number3 = 0;
@@ -24,6 +40,10 @@ public class BaseballGame {
 
         return "" + number1 + number2 + number3;
 
-
     }
+
+    public static Boolean IsCorrect(String targetNumber, String inputNumber) {
+        return targetNumber.equals(inputNumber);
+    }
+
 }
--- src/test/java/study/BaseballGameTest.java
+++ src/test/java/study/BaseballGameTest.java
@@ -2,6 +2,9 @@ package study;
 
 import TDD.BaseballGame;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class BaseballGameTest {
@@ -21,6 +24,21 @@ public class BaseballGameTest {
         assertThat(num2).isNotEqualTo(num3);
         assertThat(num3).isNotEqualTo(num1);
 
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123"})
+    void 기능2_사용자의_숫자_입력받은_숫자와_컴퓨터의_숫자_비교(String inputNumber) {
+        // given
+        String targetNumber = BaseballGame.generateRandomNumber();
+
+        // when
+        boolean result = BaseballGame.IsCorrect(targetNumber, inputNumber);
+
+        // then
+        System.out.println("TARGET:" + targetNumber + " INPUT : " + inputNumber + " RESULT:" + result);
+        assertThat(result).isEqualTo(false);
+        //assertThat(result).isEqualTo(true);
     }
 
 }
